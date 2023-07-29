3c85a03d55874095c349edf5a9486bb6b2882b61
MichaelaŠtencl <majchala8@gmail.com>
2023-07-29 10:19:53 +0200

    fční hra s obrázky

--- end.js
+++ end.js
@@ -2,12 +2,21 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 const finalScore = document.getElementById('finalScore');
+const finalText = document.getElementById('finalText');
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const MAX_HIGH_SCORES = 5;
 
+// Výsledek - OPRAVIT JEŠTĚ
 finalScore.innerText = mostRecentScore;
+
+if (mostRecentScore <= 50) {finalText.innerHTML = '<img src="https://media.giphy.com/media/cr9vIO7NsP5cY/giphy.gif">'
++ '<p>LOL. You should definitely listen to more quality music.</p>';}
+else if (mostRecentScore <= 100) {finalText.innerHTML = '<img src="https://media.giphy.com/media/VhWVAa7rUtT3xKX6Cd/giphy.gif">'
++ '<p>Not bad at all, but you still need to educate yourself</p>';}
+else {finalText.innerHTML = 'img src="https://media.giphy.com/media/44gu1V41ejJni/giphy.gif">' + '<p>You are badass!</p>';}
+
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value; // pokud nebude zadané username, tlačítko nebude available
--- game.js
+++ game.js
@@ -7,17 +7,13 @@ let loaderWrapper = document.getElementsByClassName("loader-wrapper");
 let loader = document.getElementsByClassName("loader");
 let inner = document.getElementsByClassName("loader-inner");
 let wrapper = document.getElementById("wrapper");
+const imageContainer = document.getElementById("image-container")
 
 
 
 setTimeout(() => {
     wrapper.style.visibility = 'hidden';
 }, 3000);
-
-
-//setTimeout(function(){loaderWrapper.style.display = none}, 3000)
-//setTimeout(function(){loader.style.display = none}, 3000)
-
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -26,42 +22,136 @@ let questionCounter = 0;
 let availableQuestions = [];
 
 
-// UDĚLAT IMPORT SOUBOR 
+// QUESTIONS
 let questions = [
     {
-        question: 'What is 2 + 2?',
-        choice1: '5',
-        choice2: '4',
-        choice3: '7',
+        image: "assets/Q13.png",
+        question: "What's the name of this album?",
+        choice1: 'Mr. Wonderful',
+        choice2: 'Rumours',
+        choice3: 'Tango in the Night',
         answer: 2,
     },
     { 
-        question: 'What is 2 + 7?',
-        choice1: '9',
-        choice2: '4',
-        choice3: '7',
+        image: "assets/Q5.png",
+        question: 'This might be harder. Guess the album title.',
+        choice1: 'Sticky fingers',
+        choice2: 'Emotional Rescue',
+        choice3: 'Black and Blue',
         answer: 1,
     },
     {
-        image: 'https://miro.medium.com/v2/resize:fit:1400/format:webp/1*YQgaKfVzK-YpxyT3NYqJAg.png',
-        question: 'What is 0 + 2?',
-        choice1: '5',
-        choice2: '4',
-        choice3: '2',
+        image: 'assets/Q5.png',
+        question: 'Which band released this album?',
+        choice1: 'Led Zeppelin',
+        choice2: 'The Who',
+        choice3: 'The Rolling Stones',
         answer: 3,
     },
     {
-        image: "Návrh bez názvu.jpg",
-        question: 'What is 2 + 56?',
-        choice1: '5',
-        choice2: '58',
-        choice3: '7',
+        image: "assets/Q2.jpg",
+        question: 'Ok, this one is simple:',
+        choice1: 'Abby Road',
+        choice2: 'Abbey Road',
+        choice3: 'Abey Road',
         answer: 2,
     },
+    {
+        image: "assets/Q2.jpg",
+        question: 'Which band released this album?',
+        choice1: 'Beatles',
+        choice2: 'The Strokes',
+        choice3: 'Led Zeppelin',
+        answer: 1,
+    },
+    {
+        image: "assets/Q11.jpg",
+        question: 'Which band released this album?',
+        choice1: 'The Velvet Underground',
+        choice2: 'Andy Warhol',
+        choice3: 'The Velvet Underground & Nico',
+        answer: 3,
+    },
+    {
+        image: "assets/Q10.jpg",
+        question: 'Which band released this album?',
+        choice1: 'Joy Division',
+        choice2: 'Arctic Monkeys',
+        choice3: 'The Velvet Underground',
+        answer: 1,
+    },
+    {
+        image: "assets/Q11.jpg",
+        question: "What's the name of this Arctic Monkeys album?",
+        choice1: 'AM',
+        choice2: 'Suck it and See',
+        choice3: 'The Car',
+        answer: 1,
+    },
+    {
+        image: "assets/Q7.png",
+        question: "Who released this album?",
+        choice1: 'Bryan Adams',
+        choice2: 'Lenny Kravitz',
+        choice3: ' Bruce Springsteen',
+        answer: 3,
+    },
+    {
+        image: "assets/Q3.jpg",
+        question: "Can you guess who released this album?",
+        choice1: 'Pink Floyd',
+        choice2: 'Led Zeppelin',
+        choice3: 'The Who',
+        answer: 2,
+    },
+    // -> 10 
+    {
+        image: "assets/Q8.png",
+        question: 'Which band released this album?',
+        choice1: 'The Clash',
+        choice2: 'Sex Pistols',
+        choice3: 'The Rolling Stones',
+        answer: 1,
+    },
+    {
+        image: "assets/Q9.png",
+        question: 'What is the name of this iconic album?',
+        choice1: 'Yellow Submarine',
+        choice2: "A Hard Day's Night",
+        choice3: "Sgt Pepper’s Lonely Hearts Club Band",
+        answer: 3,
+    },
+    {
+        image: "assets/Q4.png",
+        question: 'Hey, this one is super easy: the name?',
+        choice1: 'Nevermind',
+        choice2: 'In Utero',
+        choice3: 'Bleach',
+        answer: 1,
+    },
+    {
+        image: "assets/Q6.png",
+        question: 'What is the name of this album and who released it?',
+        choice1: 'Fletwood Mac - Fleetwood Mac',
+        choice2: 'Beatles - The Beatles',
+        choice3: 'Pink Floyd - The Wall',
+        answer: 2,
+    },
+    {
+        image: "assets/Q12.png",
+        question: 'What is the name of this album and who released it?',
+        choice1: 'Pink Floyd - The Division Bell',
+        choice2: 'Pink Floyd - The Wall',
+        choice3: 'Pink Floyd - The Dark Side of the Moon',
+        answer: 3,
+    },
+    // -> 15 
+
+
 ]
 
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 4;
+const MAX_QUESTIONS = 15;
 
 startGame = () => {
     questionCounter = 0;
@@ -86,7 +176,15 @@ getNewQuestion = () => {
     // vypsání náhodné otázky
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+    question.innerHTML = currentQuestion.question;
+
+        if (currentQuestion.image) {
+        imageContainer.innerHTML = `<img src="${currentQuestion.image}" alt="Obrázek">`;
+    } else {
+        imageContainer.innerHTML = ''; // Pokud není obrázek, vymažeme obsah kontejneru
+    }
+
+
 
     // zobrazení možností k dané otázce
     choices.forEach(choice => {
